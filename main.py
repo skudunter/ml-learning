d1ae1e2367bd734fe2a5dd2cd02b3b4f9b58ceea
skudunter <danielgruttercpt@gmail.com>
2025-04-15 19:15:13 +0200

    final changes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
+from imblearn.over_sampling import SMOTE
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.metrics import confusion_matrix, classification_report 
+from sklearn.model_selection import train_test_split 
+import statsmodels.api as sm
 
-data = pd.read_csv('training_data.csv')
-data = data.drop(['Unnamed: 0'], axis=1)
-
-print(data.isnull().sum())
+data = pd.read_csv('training_data_preprocessed.csv')
 
 def outliers(column):
     Q1 = data[column].quantile(0.25)
@@ -14,4 +15,16 @@ def outliers(column):
     IQR = Q3 - Q1
     lower_bound = Q1 - 1.5 * IQR
     upper_bound = Q3 + 1.5 * IQR
-    return data[(data[column] < lower_bound) | (data[column] > upper_bound)]
+    return data[(data[column] < lower_bound) | (data[column] > upper_bound)].shape[0]
+
+X_train, X_test, y_train, y_test = train_test_split(data.drop(['y'], axis=1), data['y'], test_size=0.2, random_state=0)
+
+sm = SMOTE()
+X_train_res,y_train_res = sm.fit_resample(X_train, y_train)
+
+model = RandomForestClassifier()
+model.fit(X_train_res, y_train_res)
+predictions = model.predict(X_test)
+
+print(outliers('balance'))
+print(classification_report(y_test, predictions))
--- preproccess.py
+++ preproccess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import warnings
-from sklearn.preprocessing import Normalizer,OneHotEncoder
-from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.preprocessing import RobustScaler,OneHotEncoder
 warnings.filterwarnings('ignore')
 
 # read data
@@ -10,16 +10,33 @@ data = data.drop(['Unnamed: 0'], axis=1)
 
 categorical_data = data.select_dtypes(include=['object']).columns.tolist()
 numerical_data = data.select_dtypes(include=['int64', 'float64']).columns.tolist()
+y_data = data['y']
 
 # one-hot encoding
 encoder = OneHotEncoder(sparse_output=False)
 one_hot_encoded = pd.DataFrame(encoder.fit_transform(data[categorical_data]), columns=encoder.get_feature_names_out(categorical_data))
 data_encoded = pd.concat([data[numerical_data], one_hot_encoded], axis=1)
 
+# data_encoded['balance_log'] = np.log(data_encoded['balance'] + 8019)
+# data_encoded.drop(['balance'], axis=1, inplace=True)
+
 # normalization
-scaler = Normalizer()
+scaler = RobustScaler()
 scaled_data = scaler.fit_transform(data_encoded)
 data_encoded_normalised = pd.DataFrame(scaled_data, columns=data_encoded.columns)
+
+
+data_encoded_normalised['y'] = y_data
+data_encoded_normalised.drop(['y_no'], axis=1, inplace=True)
+data_encoded_normalised.drop(['y_yes'], axis=1, inplace=True)
+data_encoded_normalised.drop(['job_unknown'], axis=1, inplace=True)
+data_encoded_normalised.drop(['marital_divorced'], axis=1, inplace=True)
+data_encoded_normalised.drop(['education_unknown'], axis=1, inplace=True)
+data_encoded_normalised.drop(['default_yes'], axis=1, inplace=True)
+data_encoded_normalised.drop(['housing_yes'], axis=1, inplace=True)
+data_encoded_normalised.drop(['loan_yes'], axis=1, inplace=True)
+data_encoded_normalised.drop(['contact_unknown'], axis=1, inplace=True)
+data_encoded_normalised.drop(['poutcome_unknown'], axis=1, inplace=True)
 
 data_encoded_normalised.to_csv('training_data_preprocessed.csv', index=False)
 
